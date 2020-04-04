646b6051a6e4e558dadebef483a3d3d86ac1e050
challenger772 <chalenger772@outlook.com>
2020-04-05 05:02:54 +0800

    second

--- src/index.js
+++ src/index.js
@@ -8,18 +8,19 @@ class Toggle extends React.Component {
       isToggleOn: true
     };
 
-    this.handleClick = this.handleClick.bind(this);
+    
   }
+   
   
   handleClick() {
     this.setState(state => ({
       isToggleOn: !state.isToggleOn
     }));
-  }
+  };
 
   render() {
     return (
-      <button onClick={this.handleClick}>{this.state.isToggleOn ? 'On' : 'Off'}</button>
+      <button onClick={() => this.handleClick()}>{this.state.isToggleOn ? 'On' : 'Off'}</button>
     )
   }
 }
