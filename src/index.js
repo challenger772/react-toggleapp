f769bd63e5be209e1745c82d80e00f951fa0d802
challenger772 <chalenger772@outlook.com>
2020-04-05 04:46:31 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>{this.state.isToggleOn ? 'On' : 'Off'}</button>
+    )
+  }
+}
+
+ReactDom.render(
+  <Toggle />,
+  document.getElementById('root')
+)
